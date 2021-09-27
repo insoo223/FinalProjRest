8262f2e6d3302f3c22d29cf8bdb8a1eac005720f
Insoo Kim <insoo@hotmail.com>
2021-09-27 16:45:04 +0900

    search dish is working, but NOT adding to cart

--- components/dishList.js
+++ components/dishList.js
@@ -45,22 +45,48 @@ function DishList(props) {
   }) || [];
 
   let dishId = searchQuery[0] ? searchQuery[0].id : null;
-  let restId;
-  // definet renderer for Dishes
+  // let restId;
+  // define renderer for Dishes
   const renderDishes = () => {
     // return (<Dishes dishId={1}> </Dishes>)
     return (<h2>rendered dish list</h2>)
   };
 
-  return (
-    <>
-      <h2>dish list</h2>
-      {/* {alert(`restaurant.js page search word: ${props.search}`)} */}
-      <Dishes restId={props.search}> </Dishes>
-    </>
-  );
+  {/* {alert(`restaurant.js page search word: ${props.search}`)} */}
+  {/* <Dishes restId={props.search}> </Dishes> */}
+  if (searchQuery.length > 0) 
+  {
+    const dishList = searchQuery.map((res) => (
+      <Col xs="6" sm="4" key={res.id}>
+        <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
+          <CardImg
+            top={true}
+            style={{ height: 200 }}
+            src={`http://localhost:1337` + res.image.url}
+          />
+          <CardBody>
+            <CardText>{res.description}</CardText>
+          </CardBody>
+          <div className="card-footer">
+            <Button color="info" onClick={() => setRestaurantID(res.id)}>{res.name}</Button>
+          </div>
+        </Card>
+      </Col>
+    )) //const dishList = searchQuery.map((res) => (
 
+    return (
+      <Container>
+        <Row xs='3'>
+          {dishList}
+        </Row>
+      </Container>
 
+    )
+    } //if (searchQuery.length > 0) 
+    else 
+    {
+      return <h1> No Dishes Found</h1>
+    } 
 
 }
 export default DishList
